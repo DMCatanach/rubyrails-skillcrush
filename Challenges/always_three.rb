16d3addc6756e979bfa423375362e8fe5d329aeb
DMCatanach <dawn.catanach@gmail.com>
2019-07-28 18:07:13 -0600

    refactor to be a method

--- Challenges/always_three.rb
+++ Challenges/always_three.rb
@@ -1,3 +1,7 @@
-puts "Give me a number, any number."
-number = gets.to_i
-puts "Surprise, surprise, the number is: #{((((number + 5) * 2) - 4) / 2) - number}!"
+def always_three
+	puts "Give me a number, any number."
+	number = gets.to_i
+	puts "Surprise, surprise, the number is: #{((((number + 5) * 2) - 4) / 2) - number}!"
+end 
+
+always_three
